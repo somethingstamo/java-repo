b80835f3d44d28a8d5324efd7688aa8a8c228a76
owenstamo <owenstamo@me.com>
2024-02-05 23:30:28 -0800

    Adding javadoc comment

---
+++ NumberCounter.java
@@ -0,0 +1,29 @@
+public class NumberCounter {
+    public static void main(String args[]) {
+        int[] testNumbers = {3, 3, 4, 5, 3, 3};
+        int testNumber = 5;
+
+        System.out.printf("The number 3 appears %d times in testNumbers\n", count(3, testNumbers, 0));
+
+        System.out.printf("The sum all positive numbers including and below %d is %d\n", testNumber, sum(testNumber, 0));
+    }
+    
+    /**
+     * Returns number of appearances of integer n in array nums
+     * @param n
+     * @param nums
+     * @param i
+     * @return
+     */
+    public static int count(int n, int nums[], int i) {
+        if (i == nums.length) return 0;
+
+        return (nums[i] == n ? 1 : 0) + count(n, nums, i + 1);
+    }
+
+    public static int sum(int n, int i) {
+        if (i == n) return i;
+
+        return i + sum(n, i + 1);
+    }
+}
